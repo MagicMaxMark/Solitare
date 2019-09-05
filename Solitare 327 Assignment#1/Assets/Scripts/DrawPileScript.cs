4c3ac9ef745eaa553594596c5078857e68fcc997
Max <magicmaxmark@gmail.com>
2019-09-04 21:18:19 -0400

    got the array working in the Draw Code

--- Solitare 327 Assignment#1/Assets/Scripts/DrawPileScript.cs
+++ Solitare 327 Assignment#1/Assets/Scripts/DrawPileScript.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 
 public class DrawPileScript : MonoBehaviour
 {
-    bool ClickedOn = false;
+    public bool ClickedOn = false;
+    public int[] Cards;
+    public GameObject DiscardPile;
+    int i = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cards = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52 };
+        while (i <= 53)
+        {
+            Debug.Log(Cards[i]);
+            i += 1;
+        }
     }
 
     // Update is called once per frame
@@ -16,7 +24,7 @@ public class DrawPileScript : MonoBehaviour
     {
         if (ClickedOn == true)
         {
-
+            
         }
         
     }
