6e8bb9f394ea74d9eb22986c754bb54ef752635c
Max <magicmaxmark@gmail.com>
2019-09-07 00:22:07 -0400

    sets the game board up

--- Solitare 327 Assignment#1/Assets/Scripts/Deck.cs
+++ Solitare 327 Assignment#1/Assets/Scripts/Deck.cs
@@ -6,32 +6,171 @@ public class Deck : MonoBehaviour
 {
     public GameObject[] Cards;
     public GameObject[] ShuffledDeck;
+    public GameObject Foundation0;
+    public GameObject Foundation1;
+    public GameObject Foundation2;
+    public GameObject Foundation3;
+    public GameObject Discard;
     public bool DeckIsShuffled = false;
     public bool isClicked = false;
-    int foo;
+    int FoundationCardCounter;
+    int FoundationCardMax;
+    int RandNum;
 
-    int i = 0;
+    int CardCounter = 0;
     // Start is called before the first frame update
     void Start()
     {
-       while (i <= 51)
+       while (CardCounter <= 51)
         {
-            foo = Random.Range(0, Cards.Length);
-            while(Cards[foo] == null)
+            RandNum = Random.Range(0, Cards.Length);
+         while(Cards[RandNum] == null)
             {
-                foo = Random.Range(0, Cards.Length);
+                RandNum = Random.Range(0, Cards.Length);
             }
-            ShuffledDeck[i] = Cards[foo];
-            Cards[foo] = null;
-            i += 1;
+            ShuffledDeck[CardCounter] = Cards[RandNum];
+            Cards[RandNum] = null;
+            CardCounter += 1;
         }
         DeckIsShuffled = true;
+
+        FoundationCardCounter = 0;
+        FoundationCardMax = 3;
+
+        while (FoundationCardCounter < FoundationCardMax)
+        {
+            Debug.Log("hello");
+            Foundation0.GetComponent<FirstFoundationScript>().Foundation0Array[FoundationCardCounter] = ShuffledDeck[0];
+
+            CardCounter = 0;
+
+            while (CardCounter < 51)
+            {
+                ShuffledDeck[CardCounter] = ShuffledDeck[CardCounter + 1];
+                CardCounter += 1;
+            }
+
+            ShuffledDeck[51] = null;
+
+            FoundationCardCounter += 1;
+
+        }
+
+
+        FoundationCardCounter = 0;
+        FoundationCardMax = 4;
+
+        while (FoundationCardCounter < FoundationCardMax)
+        {
+            Debug.Log("hello");
+            Foundation1.GetComponent<FirstFoundationScript>().Foundation0Array[FoundationCardCounter] = ShuffledDeck[0];
+
+            CardCounter = 0;
+
+            while (CardCounter < 51)
+            {
+                ShuffledDeck[CardCounter] = ShuffledDeck[CardCounter + 1];
+                CardCounter += 1;
+            }
+
+            ShuffledDeck[51] = null;
+
+            FoundationCardCounter += 1;
+
+        }
+
+
+        FoundationCardCounter = 0;
+        FoundationCardMax = 5;
+
+        while (FoundationCardCounter < FoundationCardMax)
+        {
+            Debug.Log("hello");
+            Foundation2.GetComponent<FirstFoundationScript>().Foundation0Array[FoundationCardCounter] = ShuffledDeck[0];
+
+            CardCounter = 0;
+
+            while (CardCounter < 51)
+            {
+                ShuffledDeck[CardCounter] = ShuffledDeck[CardCounter + 1];
+                CardCounter += 1;
+            }
+
+            ShuffledDeck[51] = null;
+
+            FoundationCardCounter += 1;
+
+        }
+
+
+        FoundationCardCounter = 0;
+        FoundationCardMax = 6;
+
+        while (FoundationCardCounter < FoundationCardMax)
+        {
+            Debug.Log("hello");
+            Foundation3.GetComponent<FirstFoundationScript>().Foundation0Array[FoundationCardCounter] = ShuffledDeck[0];
+
+            CardCounter = 0;
+
+            while (CardCounter < 51)
+            {
+                ShuffledDeck[CardCounter] = ShuffledDeck[CardCounter + 1];
+                CardCounter += 1;
+            }
+
+            ShuffledDeck[51] = null;
+
+            FoundationCardCounter += 1;
+
+        }
+
+
+        FoundationCardCounter = 0;
+        FoundationCardMax = 1;
+
+        while (FoundationCardCounter < FoundationCardMax)
+        {
+            Debug.Log("hello");
+            Discard.GetComponent<FirstFoundationScript>().Foundation0Array[FoundationCardCounter] = ShuffledDeck[0];
+
+            CardCounter = 0;
+
+            while (CardCounter < 51)
+            {
+                ShuffledDeck[CardCounter] = ShuffledDeck[CardCounter + 1];
+                CardCounter += 1;
+            }
+
+            ShuffledDeck[51] = null;
+
+            FoundationCardCounter += 1;
+
+        }
+
     }
+
+
+      
 
     // Update is called once per frame
     void Update()
     {
-       if (isClicked == true)
+        if (ShuffledDeck[0] == null)
+        {
+            CardCounter = 0;
+
+            while (CardCounter < 51)
+            {
+                ShuffledDeck[CardCounter] = ShuffledDeck[CardCounter + 1];
+                CardCounter += 1;
+            }
+            ShuffledDeck[51] = null;
+        }
+            
+
+
+        if (isClicked == true)
         {
 
         }
---
+++ Solitare 327 Assignment#1/Assets/Scripts/FirstFoundationScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstFoundationScript : MonoBehaviour
+{
+    public GameObject Deck;
+    public GameObject[] Foundation0Array;
+    int FoundationCardCounter = 0;
+    int CardCounter = 0;
+    public bool Foundation0Setup = false;
+    int RenderCounter = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+            FoundationCardCounter = 0;
+
+            while (Foundation0Array[FoundationCardCounter] != null)
+            {
+                FoundationCardCounter += 1;
+            }
+
+            Debug.Log(FoundationCardCounter);
+
+        RenderCounter = 0;
+
+            while (RenderCounter <= FoundationCardCounter)
+            {
+                Foundation0Array[RenderCounter].transform.parent = gameObject.transform;
+                Foundation0Array[RenderCounter].transform.localPosition = new Vector3(RenderCounter * 0.5f, 0, 0);
+                Foundation0Array[RenderCounter].GetComponent<SpriteRenderer>().sortingOrder = 52 - RenderCounter;
+                RenderCounter += 1;
+            }
+        
+    }
+}
